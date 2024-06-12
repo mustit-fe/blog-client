74fd04492f634465b8eda09784a8a791f02e0043
주영석 <ysjoo@mustit.co.kr>
2024-06-12 12:05:19 +0900

    chore: Refactor server utils and improve article sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,23 @@ import API from '@/app/_lib/fetcher/fetcher';
 import CommonContainer from './_components/common/CommonContainer';
 import ArticleList from './_components/home/ArticleList';
 import { ARTICLE_LIST_OFFSET } from '@/app/_constants/constant/constant';
+import { convertMDXToArticle, getMDXs, sortArticles } from './_lib/utils/server/utils';
 
 export default async function Home() {
-  const response = await API.getArticles({
-    page: 1,
-    offset: ARTICLE_LIST_OFFSET,
-  });
+  const articles = await API.fetchArticles({ page: 1, offset: ARTICLE_LIST_OFFSET });
+  articles.articles = articles.articles.map(article => ({
+    ...article,
+    type: 'article',
+  }));
+  const mdxs = await getMDXs({ page: 1, offset: ARTICLE_LIST_OFFSET });
+  const mdxConverted = convertMDXToArticle(mdxs);
+  const sorted = sortArticles(articles.articles, mdxConverted);
+
+  const response = {
+    articles: sorted,
+    page: articles.page,
+    totalPage: Math.max(articles.totalPage, mdxs.hasNext ? 2 : 1),
+  };
   return (
     <CommonContainer header footer>
       <ArticleList initial={response} />
